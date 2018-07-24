47f237aa23cffd6eccf000f0aa69c64519c3d03f
Maverick Tesini <mavericktesini@gmail.com>
2018-07-24 03:49:39 -0400

    db rollback, amend migrationt tables for users and planes

--- db/migrate/20180724073830_create_planes.rb
+++ db/migrate/20180724073830_create_planes.rb
@@ -1,6 +1,11 @@
 class CreatePlanes < ActiveRecord::Migration
   def change
     create_table :planes do |t|
+      t.string :identifier
+      t.string :model
+      t.integer :serial_number
+      t.string :base
+      t.integer :user_id 
 
       t.timestamps null: false
     end
