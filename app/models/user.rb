83b0d158004fa003e2c68d9e5e2bdd8797105d13
Maverick Tesini <mavericktesini@gmail.com>
2018-07-24 18:37:56 -0400

    add slug and find by slug methods to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-  has_many :planes 
+  has_many :planes
   has_secure_password
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.detect {|user| user.slug == slug}
+  end
+  
 end
