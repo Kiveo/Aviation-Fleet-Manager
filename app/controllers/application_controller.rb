285cbe4f1eac34ce8bdbca8d69ae5d4cd7f001a8
Maverick Tesini <mavericktesini@gmail.com>
2018-07-24 20:38:13 -0400

    add error view and duplicate username avoidance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/error' do
+    erb :error
+  end
+
   helpers do
 
     def logged_in?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,12 @@ class UsersController < ApplicationController
 
   # POST: /users
   post "/users" do
+    @prior_user = User.find_by(username: params[:username])
     if params[:username] == "" || params[:password] == ""
       redirect :"/signup"
+    elsif params[:username] == @prior_user.username
+      @error_message = "Username unavailable"
+      erb :error
     else
       @user = User.create(username: params[:username], password: params[:password])
       session[:user_id] = @user.id
