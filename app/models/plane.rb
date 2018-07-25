0ff39439cdfe31cc0c5b18928a21f29ab44b067b
Maverick Tesini <mavericktesini@gmail.com>
2018-07-25 02:23:04 -0400

    add validates to user and plane models

--- app/models/plane.rb
+++ app/models/plane.rb
@@ -1,4 +1,7 @@
 class Plane < ActiveRecord::Base
   belongs_to :user
 
+  validates :identifier, presence: true
+  validates :identifier, uniqueness: true
+  validates :serial_number, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_many :planes
   has_secure_password
+  validates :username, presence: true 
+  validates :username, uniqueness: true
 
   def slug
     self.username.downcase.gsub(" ", "-")
@@ -9,5 +11,5 @@ class User < ActiveRecord::Base
   def self.find_by_slug(slug)
     User.all.detect {|user| user.slug == slug}
   end
-  
+
 end
